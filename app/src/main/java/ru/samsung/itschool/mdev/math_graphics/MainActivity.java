7f1af138d6f43a648c35bc7087aae430117fac61
Pavel <impetus@yandex.ru>
2020-06-29 14:43:03 +0300

    Example 1.8.8

---
+++ app/src/main/java/ru/samsung/itschool/mdev/math_graphics/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.samsung.itschool.mdev.math_graphics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new MyGraph(this));
+    }
+}
